bff9cb10f578f369c2f441f1e8e7d7d1881f834f
Franklyn23 <90188957+Franklyn23@users.noreply.github.com>
2021-09-06 15:09:54 +0400

    Update exe1.py
    First try at giving Variables

--- exe1.py
+++ exe1.py
@@ -7,3 +7,12 @@ print("This is my second line in code")
 print("Math Calc", 5*3)
 #trial Math
 print("Is 5 greater than 2", 5>=2)
+
+#First Variable problem 
+
+apples=10
+bananas=5
+customers=2
+
+print("Customer wants 8 apples how many are left? There are", apples-8, "left")
+print("Customer 2 wants 2 bananas how many are left? There are", bananas-2, "left")
