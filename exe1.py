0482acaccbc044e3b9357dab0e118a4ab867a025
Franklyn23 <90188957+Franklyn23@users.noreply.github.com>
2021-09-06 16:17:51 +0400

    Update exe1.py
    Using string formatters

--- exe1.py
+++ exe1.py
@@ -16,3 +16,18 @@ customers=2
 
 print("Customer wants 8 apples how many are left? There are", apples-8, "left")
 print("Customer 2 wants 2 bananas how many are left? There are", bananas-2, "left")
+
+
+#Trying out string formatters
+
+my_name= 'Franklyn'
+my_age= 26
+my_star_sign= 'Aquarius'
+my_height= 188.235
+
+print("My name is %s ." % my_name)
+print("I am %d years old." % my_age)
+print("I am an %s by birth." % my_star_sign)
+print("My height is %d cms" % (my_height))
+
+# Important lesson to remember. The book does not close all comands with parenthesis . You have to open and close all parenthesis to execute without errors.
